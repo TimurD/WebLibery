1879e108e256e06a91564544369ed2d946ce747a
Timur <timur.dadashov@gmail.com>
2016-10-18 22:49:29 +0300

    add locales and search

---
+++ src/main/java/com/java/spring/libery/enums/SearchType.java
@@ -0,0 +1,8 @@
+package com.java.spring.libery.enums;
+
+/**
+ * Created by timur_000 on 18.10.2016.
+ */
+public enum SearchType {
+    TITLE,AUTHOR;
+}
--- src/main/java/com/java/spring/libery/objects/Utils.java
+++ src/main/java/com/java/spring/libery/objects/Utils.java
@@ -1,16 +1,45 @@
 package com.java.spring.libery.objects;
 
+import com.java.spring.libery.enums.SearchType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by timur_000 on 17.10.2016.
  */
 @Component
-public class Utils {
+public class Utils{
+    private SearchType searchType=SearchType.TITLE;
+    private Map<String ,SearchType>searchTypeMap =new HashMap<String, SearchType>();
+
+    @Autowired
+    MessageSource msg;
 
     private Character[] letters = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
 
     public Character[] getLetters() {
         return letters;
     }
+
+    public SearchType getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchTypeMap(Map<String, SearchType> searchTypeMap) {
+        this.searchTypeMap = searchTypeMap;
+    }
+
+    public Map<String ,SearchType>getSearchTypeMap(){
+        searchTypeMap.clear();
+        searchTypeMap.put(msg.getMessage("author_name",null, FacesContext.getCurrentInstance().getViewRoot().getLocale()),searchType.AUTHOR);
+        searchTypeMap.put(msg.getMessage("book_name",null, FacesContext.getCurrentInstance().getViewRoot().getLocale()),searchType.TITLE);
+        return searchTypeMap;
+    }
 }
+
+
